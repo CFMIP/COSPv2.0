c6cf508cfc9b6e86e57844bad56081688f4a6829
Dustin Swales <dustin.swales@noaa.gov>
2017-12-11 10:53:19 -0700

    Cleaned up testing script.

--- driver/netCDF/test_cosp.py
+++ driver/netCDF/test_cosp.py
@@ -26,21 +26,25 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+# Python module dependencies:
+# numpy, netCDF4, argparse, warnings
+#
 # History
 # Dec 2017 - D. Swales - Original version
-# 
-##########################################################################################
 #
+# Comments:
 # The purpose of this program is to compare COSP2 diagnostics.
-#
-#                                           INPUTS
-# VARIABLE NAME         TYPE            DESCRIPTION 
+# By default, errors are defined as relative differences greater than 1e-5. This threshold
+# can be adjusted through command line arguments
+# 
+# Inputs:
+# NAME                  TYPE            DESCRIPTION 
 # fileRef               string          COSP output file (Reference)
 # fileIN                string          COSP output file
 # -zeroThresh           float           error threshold(optional)
 #
 ##########################################################################################
-import os,fnmatch,numpy,netCDF4,sys,argparse,warnings
+import numpy,netCDF4,argparse,warnings
 
 # Get command line arguments
 parser = argparse.ArgumentParser(description='Read user specifications')
@@ -60,51 +64,52 @@ warnings.filterwarnings("ignore")
 print "############################################################################################"
 print 'Treating relative differences less than {0:.10f}% as insignificant'.format(100*args.zeroThresh)
 
-# What fields to compare?
+# Extract variable names from netCDF files.
 dset    = netCDF4.Dataset(fileIN)
-varlist = dset.variables.keys()
-nvars   = len(varlist)
+varlist = dset.variables.keys()   # Variable names
+nvars   = len(varlist)            # Number of variables
 
-# Loop over all of the fields and compare to reference.
-valError = [0]*nvars
-minError = [0]*nvars
-maxError = [0]*nvars
+# Loop over each variable name and perform comparison.
+fracError = [0]*nvars             # Error fraction
+minError  = [0]*nvars             # Minimum difference
+maxError  = [0]*nvars             # Maximum difference
+count = 0
+
 for ij in range(0,nvars):
-    print varlist[ij]
-    
-    # Open files
+
+    # Read in COSP reference data
     fileID_ref = netCDF4.Dataset(fileRef,'r')
-    fileID     = netCDF4.Dataset(fileIN,'r')
+    var_ref    = fileID_ref.variables[varlist[ij]][:]
 
-    # Read in data
-    var_ref = fileID_ref.variables[varlist[ij]][:]
-    var_out = fileID.variables[varlist[ij]][:]
+    # Read in COSP data
+    fileID = netCDF4.Dataset(fileIN,'r')
+    var    = fileID.variables[varlist[ij]][:]
 
-    # Compare output data to reference data.
-    diff  = var_out/var_ref-1
+    # Compute relative difference.
+    diff  = var/var_ref-1
     diff[numpy.where(var_ref == 0.0)] = 0.0
-    #diff = numpy.reshape(diff,diff.size)        
+
+    # Flag any point which exceeds error threshold. 
     error = numpy.argwhere(abs(diff) >= args.zeroThresh)
 
-    valError[ij] = float(len(error))/diff.size
-    if (len(error) > 0):
+    # Error fraction
+    fracError[ij] = float(len(error))/diff.size
+
+    # If there are differences, gather range for differences.
+    if (fracError[ij] > 0):
+        # NOTE: The "argwhere" function only works on 1D arrays, so need to reshape first.
         diff2 = numpy.reshape(diff,diff.size)
         error2 = numpy.argwhere(abs(diff2) >= args.zeroThresh)
         minError[ij] = min(diff2[error2])
         maxError[ij] = max(diff2[error2])
-        err  = valError[ij]*100.00
-#        print "  "+(varlist[ij]+":").ljust(16)+(" %1.2f" % err).rjust(10),"% of values differ,"+\
-#                "relative range:".rjust(16)+(" %1.2e " % minError[ij]).rjust(12)+"to"+\
-#                (" %1.2e" % maxError[ij]).rjust(10)
-#        if (ij == nvars-1): print "All other fields match."
+        err  = fracError[ij]*100.00
 
-for ij in range(0,nvars):
-    if (valError[ij] > 0):
+        # Display differences to screen
+        count = count + 1
         print "  "+(varlist[ij]+":").ljust(16)+(" %1.2f" % err).rjust(10),"% of values differ,"+\
             "relative range:".rjust(16)+(" %1.2e " % minError[ij]).rjust(12)+"to"+\
             (" %1.2e" % maxError[ij]).rjust(10)
-    if (ij == nvars-1): print "All other fields match."
 
-print "############################################################################################"
-
+if (count != 0): print "All other fields match."
+if (count == 0): print "All fields match"
 # END PROGRAM
