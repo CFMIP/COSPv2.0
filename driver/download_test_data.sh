4ada02c07611015cac50aefde26c96d38af996cd
mvdebolskiy <nomatdebol@gmail.com>
2024-03-06 14:50:18 +0100

    update download_test_data

--- driver/download_test_data.sh
+++ driver/download_test_data.sh
@@ -1,4 +1,4 @@
-KGO_VERSION=v001
+KGO_VERSION=v002
 # Input: global UM
 DRIVER_DIR=${PWD}
 GDFILE='https://docs.google.com/uc?export=download&id=17eK4_DVEvFOE9Uf6siXJDpWZJKT1aqkU'
@@ -9,7 +9,7 @@ cd data/inputs/UKMO
 md5sum -c cosp_input.um_global.nc.md5
 # KGO: global UM
 cd ${DRIVER_DIR}
-GDFILE='https://docs.google.com/uc?export=download&id=1uQBPUEXlniQWEp2nU3iC6d8CO3GM9cvP'
+GDFILE='https://docs.google.com/uc?export=download&id=1s5Ha6Hqnv_hWbRUs8KQpJ4Lxy8uvJDar'
 OUTPATH=data/outputs/UKMO/cosp2_output.um_global.gfortran.kgo.$KGO_VERSION.nc.gz
 wget --no-check-certificate $GDFILE -O $OUTPATH
 gunzip ${OUTPATH}
@@ -17,9 +17,17 @@ cd data/outputs/UKMO
 md5sum -c cosp2_output.um_global.gfortran.kgo.$KGO_VERSION.nc.md5
 # KGO:  UM
 cd ${DRIVER_DIR}
-GDFILE='https://docs.google.com/uc?export=download&id=1gSEdJJpqhfElsFNcTF_r4A_0vIMEWGla'
+GDFILE='https://docs.google.com/uc?export=download&id=11dKcIL3EQr7s6jbo4f9GsoW0SufesGbq'
 OUTPATH=data/outputs/UKMO/cosp2_output_um.gfortran.kgo.$KGO_VERSION.nc.gz
 wget --no-check-certificate $GDFILE -O $OUTPATH
 gunzip ${OUTPATH}
 cd data/outputs/UKMO
 md5sum -c cosp2_output_um.gfortran.kgo.$KGO_VERSION.nc.md5
+# KGO: global model levels UM
+cd ${DRIVER_DIR}
+GDFILE='https://docs.google.com/uc?export=download&id=1kY1lRgzd0UhDiQef2u-VgTQql_iut3U2'
+OUTPATH=data/outputs/UKMO/cosp2_output.um_global_model_levels.gfortran.kgo.$KGO_VERSION.nc.gz
+wget --no-check-certificate $GDFILE -O $OUTPATH
+gunzip ${OUTPATH}
+cd data/outputs/UKMO
+md5sum -c cosp2_output.um_global_model_levels.gfortran.kgo.$KGO_VERSION.nc.md5
