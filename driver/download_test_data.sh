c397e720d58da4dc30b3ac1049f44f3eb252e90d
alejandrobodas <alejandroxtr@gmail.com>
2025-06-26 20:12:24 +0300

    Compiler option in download_test_data.sh (#119)

--- driver/download_test_data.sh
+++ driver/download_test_data.sh
@@ -1,15 +1,41 @@
 #/bin/bash
 
-KGO_VERSION=v004
+COMPILER=$1
+
+case $COMPILER in
+
+  "gfortran")
+    KGO_VERSION=v004
+    kgo_links=( "https://docs.google.com/uc?export=download&id=1kC9RViPBdAsGcOivpYXxs3hHJ11Kv8IN" \
+                "https://docs.google.com/uc?export=download&id=1X_oOzvY2lf-kyAR-D1E6JfuefkGg1idn" \
+                "https://docs.google.com/uc?export=download&id=1c14qBf9VwYJWYVGCu-Cw35F-qqHx0mSg" )
+    out_type=( "cosp2_output_um.gfortran.kgo" \
+               "cosp2_output.um_global.gfortran.kgo" \
+               "cosp2_output.um_global_model_levels.gfortran.kgo" )
+    ;;
+
+   "ifort")
+    KGO_VERSION=v005
+    kgo_links=( "https://docs.google.com/uc?export=download&id=1R6uvtWno-bhdgxACCwdcaRNXs9l5_G4w" \
+                "https://docs.google.com/uc?export=download&id=1bVU1_dx6hXcV_fHX2cLUn-D03T-Hij8z" )
+    out_type=( "cosp2_output_um.ifort.kgo" \
+               "cosp2_output.um_global.ifort.kgo" )
+    ;;
+
+   "ifx")
+    KGO_VERSION=v005
+    kgo_links=( "https://docs.google.com/uc?export=download&id=1CLPJ1ikAcGBEiZ3WAGqL-zpaPIgC__po" \
+                "https://docs.google.com/uc?export=download&id=1JOrLFbt0BYgpD0h6A3tsoRD4iTJ4jDvZ" )
+    out_type=( "cosp2_output_um.ifort.kgo" \
+               "cosp2_output.um_global.ifort.kgo" )
+    ;;
+
+  *)
+    echo -n "Invalid compiler"
+    ;;
+esac
+
 echo "KGO_VERSION=$KGO_VERSION"
-
-kgo_links=( "https://docs.google.com/uc?export=download&id=1kC9RViPBdAsGcOivpYXxs3hHJ11Kv8IN" \
-            "https://docs.google.com/uc?export=download&id=1X_oOzvY2lf-kyAR-D1E6JfuefkGg1idn" \
-            "https://docs.google.com/uc?export=download&id=1c14qBf9VwYJWYVGCu-Cw35F-qqHx0mSg" )
-
-out_type=( "cosp2_output_um.gfortran.kgo" \
-           "cosp2_output.um_global.gfortran.kgo" \
-           "cosp2_output.um_global_model_levels.gfortran.kgo" )
 
 # Input: global UM
 DRIVER_DIR=${PWD}
