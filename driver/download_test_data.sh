9af7e8c88334d7061a2a5fc04eeb2704499f7251
alejandrobodas <alejandroxtr@gmail.com>
2025-06-26 22:23:58 +0100

    Update download_test_data.sh

--- driver/download_test_data.sh
+++ driver/download_test_data.sh
@@ -9,25 +9,25 @@ case $COMPILER in
     kgo_links=( "https://docs.google.com/uc?export=download&id=1kC9RViPBdAsGcOivpYXxs3hHJ11Kv8IN" \
                 "https://docs.google.com/uc?export=download&id=1X_oOzvY2lf-kyAR-D1E6JfuefkGg1idn" \
                 "https://docs.google.com/uc?export=download&id=1c14qBf9VwYJWYVGCu-Cw35F-qqHx0mSg" )
-    out_type=( "cosp2_output_um.gfortran.kgo" \
-               "cosp2_output.um_global.gfortran.kgo" \
-               "cosp2_output.um_global_model_levels.gfortran.kgo" )
+    out_type=( "cosp2_output_um.${COMPILER}.kgo" \
+               "cosp2_output.um_global.${COMPILER}.kgo" \
+               "cosp2_output.um_global_model_levels.${COMPILER}.kgo" )
     ;;
 
    "ifort")
     KGO_VERSION=v005
     kgo_links=( "https://docs.google.com/uc?export=download&id=1R6uvtWno-bhdgxACCwdcaRNXs9l5_G4w" \
                 "https://docs.google.com/uc?export=download&id=1bVU1_dx6hXcV_fHX2cLUn-D03T-Hij8z" )
-    out_type=( "cosp2_output_um.ifort.kgo" \
-               "cosp2_output.um_global.ifort.kgo" )
+    out_type=( "cosp2_output_um.${COMPILER}.kgo" \
+               "cosp2_output.um_global.${COMPILER}.kgo" )
     ;;
 
    "ifx")
     KGO_VERSION=v005
     kgo_links=( "https://docs.google.com/uc?export=download&id=1CLPJ1ikAcGBEiZ3WAGqL-zpaPIgC__po" \
                 "https://docs.google.com/uc?export=download&id=1JOrLFbt0BYgpD0h6A3tsoRD4iTJ4jDvZ" )
-    out_type=( "cosp2_output_um.ifort.kgo" \
-               "cosp2_output.um_global.ifort.kgo" )
+    out_type=( "cosp2_output_um.${COMPILER}.kgo" \
+               "cosp2_output.um_global.${COMPILER}.kgo" )
     ;;
 
   *)
