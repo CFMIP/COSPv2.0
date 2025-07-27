bb73219878df94a9582f23153468cfa258de32c8
alejandrobodas <alejandroxtr@gmail.com>
2025-07-27 20:11:03 +0300

    Update gfortran KGOs to v005. No cloudsat precip frequency for model level test.

--- driver/download_test_data.sh
+++ driver/download_test_data.sh
@@ -5,10 +5,10 @@ COMPILER=$1
 case $COMPILER in
 
   "gfortran")
-    KGO_VERSION=v004
-    kgo_links=( "https://docs.google.com/uc?export=download&id=1kC9RViPBdAsGcOivpYXxs3hHJ11Kv8IN" \
-                "https://docs.google.com/uc?export=download&id=1X_oOzvY2lf-kyAR-D1E6JfuefkGg1idn" \
-                "https://docs.google.com/uc?export=download&id=1c14qBf9VwYJWYVGCu-Cw35F-qqHx0mSg" )
+    KGO_VERSION=v005
+    kgo_links=( "https://docs.google.com/uc?export=download&id=15_oNojYM_QCjwA0kj2Ve4HDxy9H4m91J" \
+                "https://docs.google.com/uc?export=download&id=1fzFzXzLcYajfEmYmSj3Txw3d9gNGXFLe" \
+                "https://docs.google.com/uc?export=download&id=1F45ty8ZtuUvBSBS-hESq6avEugHzYeBa" )
     out_type=( "cosp2_output_um.${COMPILER}.kgo" \
                "cosp2_output.um_global.${COMPILER}.kgo" \
                "cosp2_output.um_global_model_levels.${COMPILER}.kgo" )
