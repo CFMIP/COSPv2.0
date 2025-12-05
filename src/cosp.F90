8b415e35398926b9aa1879334eb5224d6a415bee
jshaw35 <jonah.k.shaw@gmail.com>
2025-12-05 11:47:42 -0700

    Clean up print statements

--- src/cosp.F90
+++ src/cosp.F90
@@ -790,17 +790,12 @@ CONTAINS
                              isccp_boxtau(:,:),isccp_boxptop(:,:),                       &
                              isccp_boxttop(:,:),isccp_meantbclr(:))
        ! Store output (if requested)
-      !  print*,'After icarus_subcolumn'
-      !  print*,'isccp_boxtau(120, :):   ',isccp_boxtau(120, :)
-      !  print*,'isccp_boxptop(120, :):   ',isccp_boxptop(120, :)
        if (associated(cospOUT%isccp_boxtau)) then
           cospOUT%isccp_boxtau(ij:ik,:)  = isccp_boxtau
        endif
-      !  print*,'cospOUT%isccp_boxtau(120, :):   ',cospOUT%isccp_boxtau(120, :)
        if (associated(cospOUT%isccp_boxptop)) then
           cospOUT%isccp_boxptop(ij:ik,:) = isccp_boxptop
        endif
-      !  print*,'cospOUT%isccp_boxptop(120, :):   ',cospOUT%isccp_boxptop(120, :)
        if (associated(cospOUT%isccp_meantbclr)) then
           cospOUT%isccp_meantbclr(ij:ik) = isccp_meantbclr
        endif
@@ -967,10 +962,6 @@ CONTAINS
 
        ! Call simulator
        ! Check if isccp_boxtau values have been modified
-       print*, 'Before icarus_column'
-       print*,'isccp_boxtau(120, :):   ',isccp_boxtau(120, :)
-       print*,'isccp_boxptop(120, :):   ',isccp_boxptop(120, :)
-      !  print*,'cospOUT%isccp_boxtau(120, :):   ',cospOUT%isccp_boxtau(120, :)
        call icarus_column(isccpIN%npoints, isccpIN%ncolumns,isccp_boxtau(:,:),           &
                           isccp_boxptop(:,:)/100._wp, isccpIN%sunlit,isccp_boxttop,      &
                           cospOUT%isccp_fq(ij:ik,:,:),                                   &
@@ -978,7 +969,6 @@ CONTAINS
                           cospOUT%isccp_meanptop(ij:ik),cospOUT%isccp_meantaucld(ij:ik), &
                           cospOUT%isccp_totalcldarea(ij:ik),cospOUT%isccp_meantb(ij:ik))
        cospOUT%isccp_fq(ij:ik,:,:) = cospOUT%isccp_fq(ij:ik,:,7:1:-1)
-       print*,'cospOUT%isccp_totalcldarea(100:150):   ',cospOUT%isccp_totalcldarea(100:150)
 
        ! Check if there is any value slightly greater than 1
        where ((cospOUT%isccp_totalcldarea > 1.0-1.e-5) .and.                             &
