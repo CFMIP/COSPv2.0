c5691266093ef071bbb202ffe41767bc6c6f4a36
yuhi-n <yuhi.met.nk@gmail.com>
2026-04-10 14:38:52 +0900

    update 260410

--- subsample_and_optics_example/optics/cosp_utils.F90
+++ subsample_and_optics_example/optics/cosp_utils.F90
@@ -36,56 +36,13 @@ MODULE MOD_COSP_UTILS
   USE MOD_COSP_CONFIG
   USE mod_quickbeam_optics, only: size_distribution
   use mod_cosp_error, only: errorMessage
+  use cosp_math_constants, only: pi
   IMPLICIT NONE
 
 CONTAINS
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 !------------------- SUBROUTINE COSP_PRECIP_MXRATIO --------------
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-! SUBROUTINE COSP_PRECIP_MXRATIO(Npoints,Nlevels,Ncolumns,p,T,prec_frac,prec_type, &
-!                           n_ax,n_bx,alpha_x,c_x,d_x,g_x,a_x,b_x,gamma1,gamma2,gamma3,gamma4, &
-!                           flux,mxratio,reff)
-
-!     ! Input arguments, (IN)
-!     integer,intent(in) :: Npoints,Nlevels,Ncolumns
-!     real(wp),intent(in),dimension(Npoints,Nlevels) :: p,T,flux
-!     real(wp),intent(in),dimension(Npoints,Ncolumns,Nlevels) :: prec_frac
-!     real(wp),intent(in) :: n_ax,n_bx,alpha_x,c_x,d_x,g_x,a_x,b_x,gamma1,gamma2,gamma3,gamma4,prec_type
-!     ! Input arguments, (OUT)
-!     real(wp),intent(out),dimension(Npoints,Ncolumns,Nlevels) :: mxratio
-!     real(wp),intent(inout),dimension(Npoints,Ncolumns,Nlevels) :: reff
-!     ! Local variables
-!     integer :: i,j,k
-!     real(wp) :: sigma,one_over_xip1,xi,rho0,rho,lambda_x,gamma_4_3_2,delta
-    
-!     mxratio = 0.0
-
-!     if (n_ax >= 0.0) then ! N_ax is used to control which hydrometeors need to be computed
-!         xi      = d_x/(alpha_x + b_x - n_bx + 1._wp)
-!         rho0    = 1.29_wp
-!         sigma   = (gamma2/(gamma1*c_x))*(n_ax*a_x*gamma2)**xi
-!         one_over_xip1 = 1._wp/(xi + 1._wp)
-!         gamma_4_3_2 = 0.5_wp*gamma4/gamma3
-!         delta = (alpha_x + b_x + d_x - n_bx + 1._wp)
-!         do k=1,Nlevels
-!             do j=1,Ncolumns
-!                 do i=1,Npoints
-!                     if ((prec_frac(i,j,k)==prec_type).or.(prec_frac(i,j,k)==3.)) then
-!                         rho = p(i,k)/(287.05_wp*T(i,k))
-!                         mxratio(i,j,k)=(flux(i,k)*((rho/rho0)**g_x)*sigma)**one_over_xip1
-!                         mxratio(i,j,k)=mxratio(i,j,k)/rho
-!                         ! Compute effective radius
-!                         if ((reff(i,j,k) <= 0._wp).and.(flux(i,k) /= 0._wp)) then
-!                            lambda_x = (a_x*c_x*((rho0/rho)**g_x)*n_ax*gamma1/flux(i,k))**(1._wp/delta)
-!                            reff(i,j,k) = gamma_4_3_2/lambda_x
-!                         endif
-!                     endif
-!                 enddo
-!             enddo
-!         enddo
-!     endif
-! END SUBROUTINE COSP_PRECIP_MXRATIO
-
   SUBROUTINE COSP_PRECIP_MXRATIO(Npoints,Nlevels,Ncolumns, &
                                  p,T,prec_frac,prec_type, index, sd, &
                                  flux, mxratio, reff)
@@ -99,7 +56,6 @@ CONTAINS
     real(wp),intent(out),dimension(Npoints,Ncolumns,Nlevels) :: mxratio
     real(wp),intent(inout),dimension(Npoints,Ncolumns,Nlevels) :: reff
     ! Local variables
-    real(wp) :: pi = 3.14159265358979323846264338327950288419717_wp
     integer  :: i,j,k
     real(wp) :: sigma,one_over_xip1,xi,rho0,rho,lambda_x,gamma_4_3_2,delta
     real(wp) :: apm,bpm,mu,D0,Dm  ! modified gamma DSD
